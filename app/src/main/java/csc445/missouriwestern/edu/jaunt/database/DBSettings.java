f289c456a56013b92ff1b71a79150393c8b7a5b2
Baoqiang Yan <byan@missouriwestern.edu>
2018-03-05 15:45:19 -0600

    1. Restored fragment_empty.xml 2. Created a new activity to hold Alex's delivery request screen. 3. Added a new activity for lost password

--- app/src/main/java/csc445/missouriwestern/edu/jaunt/database/DBSettings.java
+++
@@ -1,9 +0,0 @@
-package csc445.missouriwestern.edu.jaunt.database;
-
-/**
- * Created by byan on 10/8/2017.
- */
-
-public class DBSettings {
-    static final String SERVER_URL = "http://ec2-34-211-83-145.us-west-2.compute.amazonaws.com/android";
-}
