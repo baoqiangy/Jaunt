e0379ab9ba19efb050a49420d01e7fb28df5079b
Baoqiang Yan <byan@missouriwestern.edu>
2018-03-07 22:11:18 -0600

    Fixed the no-network-response issue on server side. It was caused by a debugging message sent back to the app code.  Also the caching in Glide is working properly. A variable uploadingProfilePhoto is introduced to avoid the image flashing during and after upload.

---
+++ app/src/main/java/csc445/missouriwestern/edu/jaunt/thirdparty/imageuploader/LruBitmapCache.java
@@ -0,0 +1,10 @@
+package csc445.missouriwestern.edu.jaunt.thirdparty.imageuploader;
+
+import android.support.v4.util.LruCache;
+
+/**
+ * Created by byan on 3/6/2018.
+ */
+
+public class LruBitmapCache extends LruCache {
+}
